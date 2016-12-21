6dd7dcbe1cdafacc82b1fe7677d7875b307075e5
Marwane Beriich <marber.mb11@gmail.com>
2016-12-21 20:55:13 +0000

    Create the main.js

---
+++ main.js
@@ -0,0 +1,24 @@
+function setup(){
+function map(value,b,v,b2,v2){
+  return value*((b2+v2)/(b+v));
+}
+var ab = [17.5, 9, 20, 13, 12.75, 9, 10, 10];
+var a=ab.slice();
+var maxN = a.sort(function(a,b){return b-a;})[0];
+		 
+			var boxes = [];
+			var board = document.getElementById("box");
+			var speed =3;
+			var w = 1,width;	
+	for(var i=0;i<a.length;i++){
+		width = map(ab[i],0,20,0,90);
+		boxes[i]=document.createElement("DIV");
+		boxes[i].className = "box";
+		boxes[i].innerHTML = ab[i];
+		boxes[i].style.width = width+"%";
+	}
+	for(var i=0;i<boxes.length;i++){
+		board.appendChild(boxes[i]);
+		
+	}
+}
