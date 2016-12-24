19a67654a8591a5178ad6d732f292cacdde51199
Marwane Beriich <marber.mb11@gmail.com>
2016-12-24 22:53:36 +0000

    update the Stracture of main.js

--- js/main.js
+++ js/main.js
@@ -1,24 +1,39 @@
-function setup(){
+// Define of All Variables.
+var mainArray = [],
+		copyArray ,
+		maxValue 	= 0,
+		cols 			= [],
+		widthCols = 0,
+		board	;
+
+// Define of Map Function.
 function map(value,b,v,b2,v2){
-  return value*((b2+v2)/(b+v));
+	  return value*((b2+v2)/(b+v));
 }
-var ab = [17.5, 9, 20, 13, 12.75, 9, 10, 10];
-var a=ab.slice();
-var maxN = a.sort(function(a,b){return b-a;})[0];
-		 
-			var boxes = [];
-			var board = document.getElementById("box");
-			var speed =3;
-			var w = 1,width;	
-	for(var i=0;i<a.length;i++){
-		width = map(ab[i],0,20,0,100);
+
+// Define of Setup Function.
+function setup(){
+	// Setting of Values in variables .
+	mainArray = [17.5, 9, 20, 13, 12.75, 9, 10, 10];
+	copyArray = mainArray.slice();
+	// Get the max value from Array
+	maxValue = copyArray.sort(function(a,b){return b-a;})[0];
+	board = document.getElementById("box");
+	for(var i=0;i<copyArray.length;i++){
+		//	Creating of cols, and Setting attributes(className ,width ,value) of each cols .
 		boxes[i]=document.createElement("DIV");
 		boxes[i].className = "box";
 		boxes[i].innerHTML = ab[i];
+		// Mapping of width .
+		widthCols = map(mainArray[i],0,20,0,100);
 		boxes[i].style.width ="calc("+ width +"% -10px)";
 	}
+}
+
+// Define of Show Function.
+function show(){
 	for(var i=0;i<boxes.length;i++){
+		// Adding the cols to board
 		board.appendChild(boxes[i]);
-		
-	}
+	}	
 }
